88d8303e60806cd5352e9a9dd2d7ea3d265226fa
Fernando G. Albornoz <fgalbornoz@outlook.com>
2018-02-06 10:44:16 -0300

    New Form

---
+++ WindowsFormsApplication3/UI/v_cuenta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3.UI
+{
+    public partial class v_cuenta : Form
+    {
+        public v_cuenta()
+        {
+            InitializeComponent();
+            // Algo de codigo comentado para ver si me deja hacer commit
+            if (1 == 1)
+            { }
+
+        }
+    }
+}
