7c7eb494d6b608fff54d6b8fce31cdf299ac139e
Peti Racedo <petimag@hotmail.com>
2018-02-21 12:02:20 -0300

    implementar smtp

--- WindowsFormsApplication3/LogicaNegocio/ControladorSMTP.cs
+++ WindowsFormsApplication3/LogicaNegocio/ControladorSMTP.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Mail;
 
 namespace FlyMail
 {
     class ControladorSMTP
     {
+        public void EnviarMail(SMTP pSmtp, Mail pMail)
+        {
+            try
+            {
+                SmtpClient _cliente = new SmtpClient(pSmtp.Ip, pSmtp.Puerto)
+                {
+                    EnableSsl = true,
+                    Timeout = 100000,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new System.Net.NetworkCredential(pSmtp.Usuario, pSmtp.Contraseña)
+                };
+
+                MailMessage _mensaje = new MailMessage(pMail.Remitente, pMail.Destinatario, pMail.Asunto, pMail.Mensaje);
+                _mensaje.BodyEncoding = UTF8Encoding.UTF8;
+                _mensaje.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+                _cliente.Send(_mensaje);
+            }
+
+            catch (Exception ex)
+            {
+                //si ocurre una excepción regresamos null, es importante que cachen las excepciones, yo
+                //lo hice general por modo de ejemplo
+            }
+        }
     }
 }
---
+++ WindowsFormsApplication3/LogicaNegocio/SMTP.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace FlyMail
+{
+    class SMTP
+    {
+        //usuario/mail de gmail
+        private string _usuario;
+
+        //password
+        private string _contraseña;
+
+        //el puerto para pop de gmail es el 995
+        private int _puerto;
+        //el host de pop de gmail es pop.gmail.com
+        private string _ip;
+
+        public string Usuario
+        {
+            get { return _usuario; }
+            set { _usuario = value; }
+        }
+
+        public string Contraseña
+        {
+            get { return _contraseña; }
+            set { _contraseña = value; }
+        }
+
+        public int Puerto
+        {
+            get { return _puerto; }
+            set { _puerto = value; }
+        }
+
+        public string Ip
+        {
+            get { return _ip; }
+            set { _ip = value; }
+        }
+
+        public SMTP(string pUsuario, string pContraseña, int pPuerto, string pIp)
+        {
+            _usuario = pUsuario;
+            _contraseña = pContraseña;
+            _puerto = pPuerto;
+            _ip = pIp;
+        }
+    }
+}
